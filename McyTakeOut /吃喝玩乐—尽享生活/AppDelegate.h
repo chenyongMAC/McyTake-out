bc0bf320e36c16718f66d872dc2a38893df8afe5
chenyongMAC <290363831@qq.com>
2015-10-28 20:13:30 +0800

    add files

---
+++ McyTakeOut /吃喝玩乐—尽享生活/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  吃喝玩乐—尽享生活
+//
+//  Created by mac on 15/10/11.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ McyTakeOut /吃喝玩乐—尽享生活/Classify/View/CategoryViewCell.m
@@ -0,0 +1,36 @@
+//
+//  CategoryViewCell.m
+//  吃喝玩乐—尽享生活
+//
+//  Created by mac on 15/10/19.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import "CategoryViewCell.h"
+
+@implementation CategoryViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setModel:(CategoryModel *)model {
+    
+    if (_model != model) {
+        
+        _model = model;
+        
+        [self setNeedsLayout];
+    }
+}
+
+- (void)layoutSubviews {
+    
+    [super layoutSubviews];
+    
+    [_categoryImgView sd_setImageWithURL:[NSURL URLWithString:_model.categoryPic]];
+    
+    _categoryLabel.text = _model.categoryName;
+}
+
+@end
